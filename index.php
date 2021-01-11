3407d9f5091d4aef17bc7f31d0de0f140c7f9039
sourceCodeISA <isabel.gogov1@libero.it>
2021-01-11 19:24:04 +0100

    svolto usando un for

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 
     <?php
         include "data.php";
-        include "logica.php";
     ?>
 
     <style>
@@ -24,6 +23,31 @@
 https://www.codepile.net/pile/OEWY7Q1G
 Stampare tutti i nostri hotel con tutti i dati disponibili.
 Avremo un file PHP con il nostro "database" e un file con tutta la logica. -->
+
+<h1>Hotels</h1>
+
+<?php
+
+    for ($i=0; $i < count($hotels); $i++) { 
+        $hotel = $hotels[$i];
+
+        echo 'Nome: ' . $hotel['name'] . '<br>';
+        echo 'Descrizione: ' . $hotel['description'] . '<br>';
+
+        if ($hotel['parking']) {
+            echo 'Parcheggio: Sì' . '<br>';
+        } else {
+            echo 'Parcheggio: No' . '<br>';
+        }
+
+        echo 'voto: ' . $hotel['vote'] . '/5' . '<br>';
+        echo 'Distanza dal centro: ' . $hotel['distance_to_center'] . "km" . '<br>';
+
+        echo '<br>';
+    }
+
+    
+?>
 
 </body>
 </html>
